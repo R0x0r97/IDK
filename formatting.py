2134c6dd99b8af943c45652b9f51e64b529030b7
okpeter97 <peter.olahkatai@gmail.com>
2018-12-16 15:23:01 +0200

    refactoring, improvements to resizing, anti-aliasing

--- formatting.py
+++ formatting.py
@@ -1,60 +1,68 @@
 from PIL import Image
 from array import array
-from numpy import *
+from numpy import savetxt
+from numpy import zeros
+from math import floor
 
-def imageToMatrix(imagePath, outputName, matrixSize = (28, 28)):
-	
-	data = zeros(matrixSize)
+def applyAntiAlias(image):
+	(width, height) = image.size
+	image = image.resize((width * 2, height * 2))
+	image = image.resize((width, height), Image.ANTIALIAS)
+	return image
 
-	image = Image.open(imagePath)
+def trim(image, borderScale):
+	(width, height) = image.size
 	pixels = image.load()
 
-	(width, height) = image.size
-
-	point_x_min = width
-	point_y_min = height
-	point_x_max = 0
-	point_y_max = 0
+	topLeft_x = width
+	topLeft_y = height
+	bottomRight_x = 0
+	bottomRight_y = 0
 
 	for x in range(width):
 		for y in range(height):
 			if (pixels[x, y][0] != 255):
-				if (x < point_x_min):
-					point_x_min = x
-				if (y < point_y_min):
-					point_y_min = y
-				if (x > point_x_max):
-					point_x_max = x
-				if (y > point_y_max):
-					point_y_max = y
+				if (x < topLeft_x):
+					topLeft_x = x
+				if (y < topLeft_y):
+					topLeft_y = y
+				if (x > bottomRight_x):
+					bottomRight_x = x
+				if (y > bottomRight_y):
+					bottomRight_y = y
 
-	area = (point_x_min, point_y_min, point_x_max, point_y_max)
-	print(area)
-
-	new_width = point_x_max - point_x_min
-	new_height = point_y_max - point_y_min
-	print(new_width)
-	print(new_height)
-
+	new_width = bottomRight_x - topLeft_x
+	new_height = bottomRight_y - topLeft_y
 	size = 0
 
 	if (new_width > new_height):
-		point_y_min = point_y_min - (new_width - new_height) / 2
-		point_y_max = point_y_max + (new_width - new_height) / 2
+		topLeft_y = topLeft_y - (new_width - new_height) / 2
+		bottomRight_y = bottomRight_y + (new_width - new_height) / 2
 		size = new_width
 	else:
-		point_x_min = point_x_min - (new_height - new_width) / 2
-		point_x_max = point_x_max + (new_height - new_width) / 2
+		topLeft_x = topLeft_x - (new_height - new_width) / 2
+		bottomRight_x = bottomRight_x + (new_height - new_width) / 2
 		size = new_height
 
-	area = (point_x_min, point_y_min, point_x_max, point_y_max)
-	print(area)
+	border = floor(min(topLeft_x, topLeft_y, width - bottomRight_x, height - bottomRight_y))
 
-	image_cropped = image.crop(area).resize(matrixSize, Image.ANTIALIAS)
-	pixels = image_cropped.load()
+	if (border > floor(size / borderScale)):
+		border = floor(size / borderScale)
+
+	area = (topLeft_x - border, topLeft_y - border, bottomRight_x + border, bottomRight_y + border)
+
+	return image.crop(area)
+
+def PNGToIDX(imagePath, outputName, matrixSize = (28, 28)):
+	
+	data = zeros(matrixSize)
+
+	pixels = trim(applyAntiAlias(Image.open(imagePath)), 10).resize(matrixSize).load()
 
 	for x in range(matrixSize[0]):
 		for y in range(matrixSize[1]):
 			data[x, y] = pixels[y, x][0]
 
 	savetxt(outputName, data, "%4i")
+
+PNGToIDX("number.png", "matrix.txt")
